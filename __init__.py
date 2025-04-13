1b07808e0ca0ad5c54e436fc1113b0e503c355c3
Arthur Colle <arthur@collegroup.com>
2025-04-13 16:51:00 -0400

    fix

---
+++ __init__.py
@@ -0,0 +1,7 @@
+"""Fortitude Framework
+
+A web framework that enables server-side components defined as Pydantic models,
+which can be used as input and output schemas for endpoints registered as tools.
+"""
+
+__version__ = "0.1.0"
